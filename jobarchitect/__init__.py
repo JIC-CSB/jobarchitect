dbb62ecc824488390fed753ea22ff09b10cd9dce
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-01-30 15:59:07 +0000

    Implement split_dataset function

--- jobarchitect/__init__.py
+++ jobarchitect/__init__.py
@@ -16,3 +16,24 @@ def path_from_hash(dataset_path, hash_str):
         if item["hash"] == hash_str:
             return os.path.join(data_path, item["path"])
     raise(KeyError("File hash not in dataset"))
+
+
+def split_dataset(dataset_path, nchunks):
+    """Return list of list of file entries."""
+    dataset_path = os.path.abspath(dataset_path)
+    dataset = DataSet.from_path(dataset_path)
+
+    file_list = dataset.manifest["file_list"]
+    num_files = len(file_list)
+    chunk_size = num_files // nchunks
+    left_over_files = num_files % nchunks
+    index = 0
+    for n in range(nchunks, 0, -1):
+        chunk = []
+        for i in range(chunk_size):
+            chunk.append(file_list[index])
+            index += 1
+        if n <= left_over_files:
+            chunk.append(file_list[index])
+            index += 1
+        yield chunk
--- tests/test_jobarchitect_api.py
+++ tests/test_jobarchitect_api.py
@@ -1,6 +1,7 @@
 """Test the jobarchitect package."""
 
 import os
+import json
 
 import pytest
 
@@ -26,3 +27,67 @@ def test_path_from_hash():
 
     with pytest.raises(KeyError):
         path_from_hash(TEST_SAMPLE_DATASET, "nonsense")
+
+
+def test_split_dataset():
+    from jobarchitect import split_dataset
+
+    # The function returns an iterable.
+    import collections
+    assert isinstance(
+        split_dataset(dataset_path=TEST_SAMPLE_DATASET,
+                      nchunks=1),
+        collections.Iterable)
+
+    manifest_path = os.path.join(
+        TEST_SAMPLE_DATASET, ".dtool", "manifest.json")
+    with open(manifest_path) as fh:
+        manifest = json.load(fh)
+    file_list = manifest["file_list"]
+
+    assert len(file_list) == 7
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 7)
+    assert next(tmp_iter) == [file_list[0]]
+    assert next(tmp_iter) == [file_list[1]]
+    assert next(tmp_iter) == [file_list[2]]
+    assert next(tmp_iter) == [file_list[3]]
+    assert next(tmp_iter) == [file_list[4]]
+    assert next(tmp_iter) == [file_list[5]]
+    assert next(tmp_iter) == [file_list[6]]
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 6)
+    assert next(tmp_iter) == [file_list[0]]
+    assert next(tmp_iter) == [file_list[1]]
+    assert next(tmp_iter) == [file_list[2]]
+    assert next(tmp_iter) == [file_list[3]]
+    assert next(tmp_iter) == [file_list[4]]
+    assert next(tmp_iter) == [file_list[5], file_list[6]]
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 5)
+    assert next(tmp_iter) == [file_list[0]]
+    assert next(tmp_iter) == [file_list[1]]
+    assert next(tmp_iter) == [file_list[2]]
+    assert next(tmp_iter) == [file_list[3], file_list[4]]
+    assert next(tmp_iter) == [file_list[5], file_list[6]]
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 4)
+    assert next(tmp_iter) == [file_list[0]]
+    assert next(tmp_iter) == [file_list[1], file_list[2]]
+    assert next(tmp_iter) == [file_list[3], file_list[4]]
+    assert next(tmp_iter) == [file_list[5], file_list[6]]
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 3)
+    assert next(tmp_iter) == [file_list[0], file_list[1]]
+    assert next(tmp_iter) == [file_list[2], file_list[3]]
+    assert next(tmp_iter) == [file_list[4], file_list[5], file_list[6]]
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 2)
+    assert next(tmp_iter) == [file_list[0], file_list[1], file_list[2]]
+    assert next(tmp_iter) == [file_list[3], file_list[4], file_list[5],
+                              file_list[6]]
+
+    tmp_iter = split_dataset(TEST_SAMPLE_DATASET, 1)
+    assert next(tmp_iter) == [file_list[0], file_list[1], file_list[2],
+                              file_list[3], file_list[4], file_list[5],
+                              file_list[6]]
