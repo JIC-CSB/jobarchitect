a2d98f7e04d2e8f2fc61f2e9f54efce57378abf3
Matthew Hartley <mhartley@cantab.net>
2017-02-02 15:12:12 +0000

    Add new files and tests and move generate_bash_job into backends

---
+++ jobarchitect/backends.py
@@ -0,0 +1,24 @@
+"""Job output backends."""
+
+
+def generate_bash_job(jobspec):
+    """Return bash script to run all analysis on all data in one chunk from a
+    split dataset.
+
+    jobspec should include:
+
+    dataset_path: Path to dataset
+    output_root: Root of path in which output will be written
+    program_template: Template to specify analysis program to execute
+    chunks: List of hashes to analyse
+    """
+
+    output = """#!/bin/bash
+_analyse_by_ids \
+  --program_template={program_template} \
+  --input_dataset_path={dataset_path} \
+  --output_root={output_root} \
+  {hash_ids}
+    """.format(**jobspec._asdict())
+
+    return output
---
+++ tests/test_jobarchitect_cli.py
@@ -0,0 +1,11 @@
+"""Test jobarchitect CLI (sketchjob)"""
+
+import subprocess
+
+
+# def test_sketchjob():
+
+#     cmd = ['sketchjob',
+#            '--help']
+
+#     subprocess.check_call(cmd)
