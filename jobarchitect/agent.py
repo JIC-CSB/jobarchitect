ec493b61ed289599f0deb35572157c5a97165c4e
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-02-02 12:59:08 +0000

    Add output_root to Agent initialisation

--- jobarchitect/agent.py
+++ jobarchitect/agent.py
@@ -8,9 +8,10 @@ from jobarchitect import path_from_hash, output_path_from_hash
 class Agent(object):
     """Class to create commands to analyse data."""
 
-    def __init__(self, dataset_path, program_template):
+    def __init__(self, dataset_path, program_template, output_root="/tmp"):
         self.dataset_path = dataset_path
         self.program_template = program_template
+        self.output_root = output_root
 
         self.dataset = DataSet.from_path(self.dataset_path)
 
--- tests/test_agent_module.py
+++ tests/test_agent_module.py
@@ -1,6 +1,9 @@
 """Tests for agent module."""
 
+import sys
+
 from . import TEST_SAMPLE_DATASET
+from . import tmp_dir_fixture  # NOQA
 
 
 def test_agent_initialisation():
@@ -15,6 +18,8 @@ def test_agent_initialisation():
                   "output_file": "/output/test.txt"}
     assert agent.program_template.format(**dummy_dict) == expected_string
 
+    assert agent.output_root == "/tmp"
+
     from dtool import DataSet
     assert isinstance(agent.dataset, DataSet)
 
@@ -24,7 +29,8 @@ def test_create_command():
 
     program_template = "shasum {input_file} > {output_file}"
     agent = Agent(dataset_path=TEST_SAMPLE_DATASET,
-                  program_template=program_template)
+                  program_template=program_template,
+                  output_root="/tmp/output")
 
     from jobarchitect import path_from_hash, output_path_from_hash
     expected_input_path = path_from_hash(
@@ -41,3 +47,16 @@ def test_create_command():
 
     assert expected_output == agent.create_command(
         hash_str='c827a1a1a61e734828f525ae7715d9c5be591496')
+
+
+def test_run_analysis(tmp_dir_fixture):  # NOQA
+    from jobarchitect.agent import Agent
+
+    program_name = "sha1sum"
+    if sys.platform == "darwin":
+        program_name = "shasum"
+
+    program_template = program_name + " {input_file} > {output_file}"
+    agent = Agent(dataset_path=TEST_SAMPLE_DATASET,
+                  program_template=program_template,
+                  output_root=tmp_dir_fixture)
