8ad0c9bc38bcba36c0db5da0f25d38bc3b508bd0
Matthew Hartley <mhartley@cantab.net>
2017-02-06 16:18:56 +0000

    Add script to build singularity image

---
+++ deploy_singularity_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source build_docker_image.sh
+
+# Build singularity image
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ~/mnt/cluster_home/singularity/:/output \
+    --privileged \
+    -t \
+    --rm \
+    mcdocker2singularity \
+    jicscicomp/jobarchitect \
+    jobarchitect
+
