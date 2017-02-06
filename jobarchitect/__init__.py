9196012764b80fc5d0c908049ac562f7799f4c68
Matthew Hartley <mhartley@cantab.net>
2017-02-06 16:55:58 +0000

    Update version numbers for 0.2.0 release

--- jobarchitect/__init__.py
+++ jobarchitect/__init__.py
@@ -1,6 +1,6 @@
 """jobarchitect package."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 class JobSpec(object):
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = "0.1.0"
+version = "0.2.0"
 readme = open('README.rst').read()
 url = "https://github.com/JIC-CSB/jobarchitect"
 
