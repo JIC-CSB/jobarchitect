1c86cfe4868f53c1a8f914bdedb77d5df1cfbc43
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-06-15 16:02:33 +0100

    Update sample smart tools to use new interface

--- tests/sample_smart_tools/shasum.py
+++ tests/sample_smart_tools/shasum.py
@@ -1,11 +1,13 @@
+import os
 import argparse
 
 from dtoolcore import DataSet
 
-def main(dataset_path, identifier, output_path):
+def main(dataset_path, identifier, output_directory):
     dataset = DataSet.from_path(dataset_path)
     fpath = dataset.abspath_from_identifier(identifier)
-    with open(output_path, "w") as fh:
+    output_fpath = os.path.join(output_directory, os.path.basename(fpath))
+    with open(output_fpath, "w") as fh:
         shasum_string = "{} {}\n".format(identifier, fpath)
         fh.write(shasum_string)
 
@@ -13,7 +15,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset-path", required=True)
     parser.add_argument("--identifier", required=True)
-    parser.add_argument("--output-path", required=True)
+    parser.add_argument("--output-directory", required=True)
     args = parser.parse_args()
 
-    main(args.dataset_path, args.identifier, args.output_path)
+    main(args.dataset_path, args.identifier, args.output_directory)
--- tests/sample_smart_tools/utils.py
+++ tests/sample_smart_tools/utils.py
@@ -1,5 +1,7 @@
+import os
 
-def write_shasum(output_path, shasum, fpath):
-    with open(output_path, "w") as fh:
+def write_shasum(output_directory, shasum, fpath):
+    output_fpath = os.path.join(output_directory, os.path.basename(fpath))
+    with open(output_fpath, "w") as fh:
         shasum_string = "{} {}\n".format(shasum, fpath)
         fh.write(shasum_string)
